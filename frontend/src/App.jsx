fb5fbce0421a2831272f8c5009b52fe21b9de910
Riley Flynn <technologyflynnteam@gmail.com>
2019-12-01 01:27:00 -0330

    Added currency cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,15 @@ import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CurrenciesPage from "./pages/CurrenciesPage";
+import { red } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: red
+  }
+});
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -26,16 +34,24 @@ const App = () => {
 
   return (
     <Router>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Hyperion
-          </Typography>
-          <Button color="inherit" component={CurrenciesLink}>
-            Currencies
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Hyperion
+            </Typography>
+            <Button color="inherit" component={CurrenciesLink}>
+              Currencies
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route path="/">
+            <CurrenciesPage />
+          </Route>
+        </Switch>
+      </ThemeProvider>
     </Router>
   );
 };
--- frontend/src/pages/CurrenciesPage.jsx
+++ frontend/src/pages/CurrenciesPage.jsx
@@ -1,8 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/styles";
+import {
+  GridList,
+  GridListTile,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Grid
+} from "@material-ui/core";
 
-// const CurrenciesPage = (): React.FC => {
-//     const [currencies, setCurrencies] = useState([]);
+const useStyles = makeStyles(theme => ({
+  page: {
+    padding: "1rem"
+  },
+  card: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  cardContent: {
+    flex: 1
+  }
+}));
 
-//     return <
+const CurrenciesPage = () => {
+  const classes = useStyles();
+  const [currencies, setCurrencies] = useState([]);
 
-// }
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:3000/api/currencies");
+      const respJson = await response.json();
+      setCurrencies(respJson);
+    })();
+  }, []);
+
+  return (
+    <Grid
+      container
+      className={classes.page}
+      spacing={2}
+      cols={2}
+      alignItems="stretch"
+    >
+      {currencies.map(currency => (
+        <Grid item xs={12} sm={6} key={currency.shortCode}>
+          <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography variant="h5" component="h2">
+                {currency.name}
+              </Typography>
+              <Typography color="textSecondary">
+                {currency.shortCode}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" href={currency.site}>
+                Balances
+              </Button>
+              {currency.site && (
+                <Button size="small" href={currency.site}>
+                  Currency Site
+                </Button>
+              )}
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default CurrenciesPage;
