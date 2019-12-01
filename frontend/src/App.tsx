7cb84a495cd532502e53346955f050355ebdcab4
Riley Flynn <technologyflynnteam@gmail.com>
2019-11-30 22:39:20 -0330

    Add react base

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Test
+      </header>
+    </div>
+  );
+}
+
+export default App;
